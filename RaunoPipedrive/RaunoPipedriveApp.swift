4a1f7b8a337ade7f770818055f5c14c202150c64
Rauno Villberg <rauno.villberg@gmail.com>
2023-05-15 11:15:19 +0300

    Initial Commit

---
+++ RaunoPipedrive/RaunoPipedriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaunoPipedriveApp.swift
+//  RaunoPipedrive
+//
+//  Created by Rauno Villberg on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct RaunoPipedriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
